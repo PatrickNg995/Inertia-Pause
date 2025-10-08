e3c6cf154336e2f27eef3b9dfb53f17609f7c510
fortyfiveacp <nng32@my.bcit.ca>
2025-10-08 15:42:36 -0700

    Add headers to HUDView

--- Assets/Scripts/UI/HUD/HUDView.cs
+++ Assets/Scripts/UI/HUD/HUDView.cs
@@ -4,18 +4,26 @@ using UnityEngine.UI;
 
 public class HUDView : MonoBehaviour
 {
+    [field: Header("Objectives")]
     [field: SerializeField] public GameObject ObjectivesElements { get; private set; }
     [field: SerializeField] public TMP_Text LevelNameText { get; private set; }
     [field: SerializeField] public TMP_Text ScenarioObjectivesText { get; private set; }
     [field: SerializeField] public TMP_Text OptionalObjectivesText { get; private set; }
-    [field: SerializeField] public GameObject ButtonPrompts { get; private set; }
-    [field: SerializeField] public GameObject RedoPrompt { get; private set; }
-    [field: SerializeField] public TMP_Text TelemetryText { get; private set; }
-    [field: SerializeField] public TMP_Text ActionsTakenText { get; private set; }
+
+    [field: Header("Crosshairs")]
     [field: SerializeField] public Image DefaultCrosshair { get; private set; }
+
+
+    [field: Header("Interaction Prompts")]
     [field: SerializeField] public TMP_Text InteractableNameText { get; private set; }
     [field: SerializeField] public TMP_Text InteractableActionText { get; set; }
     [field: SerializeField] public GameObject PromptsLooking { get; private set; }
     [field: SerializeField] public GameObject PromptsInteracting { get; private set; }
+
+    [field: Header("Others")]
+    [field: SerializeField] public GameObject ButtonPrompts { get; private set; }
+    [field: SerializeField] public GameObject RedoPrompt { get; private set; }
+    [field: SerializeField] public TMP_Text TelemetryText { get; private set; }
+    [field: SerializeField] public TMP_Text ActionsTakenText { get; private set; }
 
 }
