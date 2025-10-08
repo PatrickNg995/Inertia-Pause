9807598be8cc4c3c0959697efa7eefd935306a78
fortyfiveacp <nng32@my.bcit.ca>
2025-10-08 15:56:26 -0700

    Add ScriptableObject for interable and tutorial info

---
+++ Assets/Scripts/ScriptableObjects/InteractableObjectInfo/InteractableObjectInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "NewInteractableObjectInfo", menuName = "Inertia Pause/Interactable Info")]
+public class InteractableObjectInfo : ScriptableObject
+{
+    public string ObjectName;
+    public string ObjectInteraction;
+    public TutorialInfo TutorialInfo;
+}
