394cc230d8f01615c12a2a0adb6b6321230b1383
Joseph Driedger <jdriedger3@my.bcit.ca>
2025-10-22 10:14:20 -0700

    Renamed variables to naming convention

--- Assets/Scripts/PauseableAnimator.cs
+++ Assets/Scripts/PauseableAnimator.cs
@@ -2,25 +2,23 @@ using UnityEngine;
 
 public class PauseableAnimator : MonoBehaviour, IPausable
 {
-    Animator animator;
-    float prevSpeed = 1f;
+    [SerializeField] Animator _animator;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
-        animator.Play(0, 0, Random.value);
-        animator.Update(0f);
+        _animator.Play(0, 0, Random.value);
+        _animator.Update(0f);
     }
 
     public void Pause()
     {
         Debug.Log("Animator has paused.");
-        animator.speed = 0f;
+        _animator.speed = 0f;
     }
 
     public void Unpause()
     {
         Debug.Log("Animator has unpaused.");
-        animator.speed = 1f;
+        _animator.speed = 1f;
     }
 }
