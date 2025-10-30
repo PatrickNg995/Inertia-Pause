d04fcd8e871b7ec27a3d332d7aff07369d58829f
fortyfiveacp <nng32@my.bcit.ca>
2025-10-29 19:05:22 -0700

    Implement tutorial presenter and view scripts

---
+++ Assets/Scripts/UI/TutorialPanelPresenter.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class TutorialPanelPresenter : MonoBehaviour
+{
+    [Header("View")]
+    [SerializeField] private TutorialPanelView _view;
+
+    private TutorialInfo _currentTutorial;
+    private int _currentPageIndex;
+
+    void Start()
+    {
+        _view.gameObject.SetActive(false);
+
+        _view.BackButton.onClick.AddListener(OnBackClicked);
+        _view.PrevPageButton.onClick.AddListener(OnPrevClicked);
+        _view.NextPageButton.onClick.AddListener(OnNextClicked);
+    }
+
+    public void OpenMenu()
+    {
+        _view.gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        _view.gameObject.SetActive(false);
+    }
+
+    public void SetTutorial(TutorialInfo tutorial)
+    {
+        _currentPageIndex = 0;
+        _currentTutorial = tutorial;
+        _view.Header.text = tutorial.ObjectName;
+    }
+
+    private void DisplayPage(int index)
+    {
+        _currentPageIndex = index;
+        _view.ContentText.text = _currentTutorial.Pages[index].Content;
+        _view.ContentImage.sprite = _currentTutorial.Pages[index].Image;
+
+        bool isFirstPage = index == 0;
+        bool isLastPage = index == _currentTutorial.Pages.Count - 1;
+
+        _view.PrevPageButton.gameObject.SetActive(!isFirstPage);
+        _view.NextPageButton.gameObject.SetActive(!isLastPage);
+    }
+
+    private void OnPrevClicked()
+    {
+        if (_currentPageIndex > 0)
+        {
+            DisplayPage(_currentPageIndex - 1);
+        }
+    }
+
+    private void OnNextClicked()
+    {
+        if (_currentPageIndex < _currentTutorial.Pages.Count - 1)
+        {
+            DisplayPage(_currentPageIndex + 1);
+        }
+    }
+
+    private void OnBackClicked()
+    {
+        CloseMenu();
+    }
+}
