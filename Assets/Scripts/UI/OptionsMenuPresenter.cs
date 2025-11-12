1ca423fb8dc3d77d02aa7279301fb2fc670ddd5d
fortyfiveacp <nng32@my.bcit.ca>
2025-11-12 14:46:31 -0800

    Complete options view

--- Assets/Scripts/UI/OptionsMenuPresenter.cs
+++ Assets/Scripts/UI/OptionsMenuPresenter.cs
@@ -4,6 +4,8 @@ public class OptionsMenuPresenter : MonoBehaviour
 {
     [SerializeField] private OptionsMenuView _view;
 
+    private bool _isVerticalSensitivity = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +16,28 @@ public class OptionsMenuPresenter : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OpenMenu()
+    {
+        _view.gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        CloseFoldouts();
+        _view.gameObject.SetActive(false);
+    }
+
+    private void CloseFoldouts()
+    {
+        _view.HorizontalSensitivityButton.CloseFoldout();
+        _view.VerticalSensitivityButton.CloseFoldout();
+        _view.FOVButton.CloseFoldout();
+        _view.MaxFramerateButton.CloseFoldout();
+        _view.ShowMetricsButton.CloseFoldout();
+        _view.MusicVolumeButton.CloseFoldout();
+        _view.SFXVolumeButton.CloseFoldout();
+        _view.ShowObjectTrajectoryButton.CloseFoldout();
     }
 }
--- Assets/Scripts/UI/OptionsMenuView.cs
+++ Assets/Scripts/UI/OptionsMenuView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,20 @@ public class OptionsMenuView : MonoBehaviour
     [field: SerializeField] public TMP_Text LevelNameText { get; private set; }
 
     [field: Header("Option Buttons")]
+    [field: SerializeField] public CustomOptionButton HorizontalSensitivityButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton VerticalSensitivityButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton FOVButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton MaxFramerateButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton ShowMetricsButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton MusicVolumeButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton SFXVolumeButton { get; private set; }
+    [field: SerializeField] public CustomOptionButton ShowObjectTrajectoryButton { get; private set; }
+
+    [field: Header("Individual Options")]
+    [field: SerializeField] public List<Button> FramerateOptions { get; private set; }
+    [field: SerializeField] public List<Button> FOVOptions { get; private set; }
+    [field: SerializeField] public List<Button> SensitivityOptions { get; private set; }
+    [field: SerializeField] public List<Button> VolumeOptions { get; private set; }
 
     [field: Header("Bottom Bar")]
     [field: SerializeField] public Button BackButton { get; private set; }
