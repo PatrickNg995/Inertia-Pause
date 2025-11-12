3dddf83bc8211a9b0e9f5b098e65d0511480defa
Joseph Driedger <jdriedger3@my.bcit.ca>
2025-11-11 19:42:27 -0800

    Reverted PausableRigidbody to original

--- Assets/Scripts/TimePausing/PausableRigidbody.cs
+++ Assets/Scripts/TimePausing/PausableRigidbody.cs
@@ -1,62 +1,35 @@
 ﻿using UnityEngine;
 
+// Get Rigidbody, then remove & add velocity depending
+// on game state
 public class PausableRigidbody : MonoBehaviour, IPausable
 {
     [SerializeField] private Rigidbody _rb;
-
     private Vector3 _savedVelocity;
-    private Vector3 _savedAngularVelocity;
-    private bool _originalIsKinematic;
 
-    private void Awake()
-    {
-        if (_rb == null)
-            _rb = GetComponent<Rigidbody>();
+    private Vector3 _pausedPosition;
+    private Quaternion _pausedRotation;
 
-        _originalIsKinematic = _rb.isKinematic;
-    }
-
-    // Pause: store velocity and make kinematic ONLY if it was dynamic
+    // Save velocity & stop movement
     public void Pause()
     {
-        if (_rb == null) return;
-
-        if (!_rb.isKinematic)
-        {
-            _savedVelocity = _rb.linearVelocity;
-            _savedAngularVelocity = _rb.angularVelocity;
-            _rb.isKinematic = true;
-        }
+        _rb.isKinematic = true;
+        _savedVelocity = _rb.linearVelocity;
     }
 
-    // Unpause: restore original mode; if it was dynamic, restore velocity
+    // Start movement & add back saved velocity
     public void Unpause()
     {
-        if (_rb == null) return;
+        _pausedPosition = transform.position;
+        _pausedRotation = transform.rotation;
 
-        // Go back to whatever this body was originally
-        _rb.isKinematic = _originalIsKinematic;
-
-        if (!_rb.isKinematic)
-        {
-            _rb.linearVelocity = _savedVelocity;
-            _rb.angularVelocity = _savedAngularVelocity;
-        }
-    }
-
-    // Only needed if you really want to snap back a dynamic body
-    public void ResetStateBeforeUnpause(Vector3 position, Quaternion rotation)
-    {
-        // Reset position and rotation to pre-unpause state.
-        transform.SetPositionAndRotation(_pausedPosition, _pausedRotation);
-
-        if (_rb == null) return;
-        _rb.position = position;
-        _rb.rotation = rotation;
+        _rb.isKinematic = false;
+        _rb.linearVelocity = _savedVelocity;
     }
 
     public void ResetStateBeforeUnpause()
     {
-        throw new System.NotImplementedException();
+        // Reset position and rotation to pre-unpause state.
+        transform.SetPositionAndRotation(_pausedPosition, _pausedRotation);
     }
 }
