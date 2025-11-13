45f5887c23864bc8576ef18c93606a9ecf4636f9
AnthonyQR <55644542+AnthonyQR@users.noreply.github.com>
2025-11-13 00:05:04 -0800

    Changed chandelier to change tags when hit so NPCs can stand on them

---
+++ Assets/Scripts/Objects/Chandelier.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Chandelier : MonoBehaviour, IPausable
+{
+    [SerializeField] GameObject _chandelierBottom;
+    [SerializeField] Rigidbody _chandelierBottomRb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _chandelierBottom.tag = "Untagged";
+    }
+
+    public void ChandelierFalling()
+    {
+        _chandelierBottom.tag = "Lethal";
+        //for (int i = 0; i < transform.childCount; i++)
+        //{
+        //    transform.GetChild(i).GetComponent<Rigidbody>().freezeRotation = false;
+        //}
+    }
+
+    public void Pause()
+    {
+        return;
+    }
+
+    public void Unpause()
+    {
+        return;
+    }
+
+    public void ResetStateBeforeUnpause()
+    {
+        //_chandelierBottom.tag = "Untagged";
+        //for (int i = 0; i < transform.childCount; i++)
+        //{
+        //    transform.GetChild(i).GetComponent<Rigidbody>().freezeRotation = true;
+        //}
+    }
+}
