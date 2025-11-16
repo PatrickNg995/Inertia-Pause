41cbf804f0de5fce73b33a56a93ad1709365b18e
Joseph Driedger <jdriedger3@my.bcit.ca>
2025-11-16 15:10:28 -0800

    Implemented Grenade Throwing animation for current state of pause

--- Assets/Scripts/TimePausing/PauseableAnimator.cs
+++ Assets/Scripts/TimePausing/PauseableAnimator.cs
@@ -3,29 +3,53 @@
 public class PauseableAnimator : MonoBehaviour, IPausable
 {
     [SerializeField] private Animator _animator;
-    private float _animationStartTime;
+    [SerializeField] private float _frameToLoad = -1;
+    [SerializeField] private float _totalFrames;
+
+    private float _animationStartTime = -1;
+    private int _pausedStateHash;
+    private float _pausedNormalizedTime;
+    private bool _isPaused;
 
     private void Awake()
     {
-        _animationStartTime = Random.value;
-        _animator.Play(0, 0, _animationStartTime);
+        if (_frameToLoad < 0 )
+            _animationStartTime = Random.value;
+        else
+            _animationStartTime = _frameToLoad / _totalFrames;
+            _animator.Play(0, 0, _animationStartTime);
         _animator.Update(0f);
     }
 
     public void Pause()
     {
+        if (_isPaused) return;
+
         Debug.Log("Animator has paused.");
+
+        // Capture current state + time.
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        _pausedStateHash = stateInfo.fullPathHash;
+        _pausedNormalizedTime = stateInfo.normalizedTime;
+
         _animator.speed = 0f;
+        _isPaused = true;
     }
 
     public void Unpause()
     {
+        if (!_isPaused) return;
+
         Debug.Log("Animator has unpaused.");
         _animator.speed = 1f;
+        _isPaused = false;
     }
 
     public void ResetStateBeforeUnpause()
     {
-        _animator.Play(0, 0, _animationStartTime);
+        if (!_isPaused) return;
+
+        _animator.Play(_pausedStateHash, 0, _pausedNormalizedTime);
+        _animator.Update(0f);
     }
 }
