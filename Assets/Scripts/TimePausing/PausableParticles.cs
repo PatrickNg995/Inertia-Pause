6f3b4543d1b5e8bb6d630b254b871e8f5d2dcc7c
Patrick Ng <patrickng995@gmail.com>
2025-11-17 16:58:00 -0800

    Added muzzle flash to guns and new gun enemy prefab that drops gun on death

--- Assets/Scripts/TimePausing/PausableParticles.cs
+++ Assets/Scripts/TimePausing/PausableParticles.cs
@@ -1,27 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-// Get Particle System, then Pause & Play them depending
-// on game state
 public class PausableParticles : MonoBehaviour, IPausable
 {
-    private ParticleSystem ps;
+    [Header("Particle System Settings")]
+    [SerializeField] private ParticleSystem _particleSystem;
 
-    private void Awake()
+    [Tooltip("If true, the pre-pause play duration will be randomly chosen between the lower and upper bounds." +
+             "If false, the duration will use the lower bound.")]
+    [SerializeField] private bool _useRandomPrePausePlayDuration = true;
+
+    [Tooltip("The lower bound for the pre-pause play duration (in seconds).")]
+    [SerializeField] private float _prePausePlayDurationLowerBound = 0.1f;
+
+    [Tooltip("The upper bound for the pre-pause play duration (in seconds).")]
+    [SerializeField] private float _prePausePlayDurationUpperBound = 0.5f;
+
+    private float _prePausePlayDuration;
+
+    public void Awake()
     {
-        ps = GetComponent<ParticleSystem>();
+        // Determine the pre-pause play duration, if not random use the lower bound.
+        if (_useRandomPrePausePlayDuration)
+        {
+            _prePausePlayDuration = Random.Range(_prePausePlayDurationLowerBound, _prePausePlayDurationUpperBound);
+        }
+        else
+        {
+            _prePausePlayDuration = _prePausePlayDurationLowerBound;
+        }
     }
+
     public void Pause()
     {
-        ps.Pause();
+        StartCoroutine(PlayAndPauseParticleSystem(_prePausePlayDuration));
     }
 
     public void Unpause()
     {
-        ps.Play();
+        _particleSystem.Play();
     }
 
     public void ResetStateBeforeUnpause()
     {
-        
+        StartCoroutine(PlayAndPauseParticleSystem(_prePausePlayDuration));
+    }
+
+    private IEnumerator PlayAndPauseParticleSystem(float durationBeforePause)
+    {
+        // Play the particle system for a duration before pausing.
+        _particleSystem.Play();
+        yield return new WaitForSeconds(durationBeforePause);
+        _particleSystem.Pause(); 
     }
 }
