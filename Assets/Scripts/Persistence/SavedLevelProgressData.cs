76b66ca0142788622c1ddc51e8c3ea89312ed136
Patrick Ng <patrickng995@gmail.com>
2025-11-18 22:59:02 -0800

    Added persistent level progress storage

---
+++ Assets/Scripts/Persistence/SavedLevelProgressData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[Serializable]
+public class SavedLevelProgressData
+{
+    public string CurrentLevelEnvironmentName;
+    public string CurrentLevelAssetsName;
+    public LevelProgressInfo[] CompletedLevelProgressInfoArray;
+
+    public SavedLevelProgressData()
+    {
+        CurrentLevelEnvironmentName = "";
+        CurrentLevelAssetsName = "";
+        CompletedLevelProgressInfoArray = new LevelProgressInfo[0];
+    }
+}
