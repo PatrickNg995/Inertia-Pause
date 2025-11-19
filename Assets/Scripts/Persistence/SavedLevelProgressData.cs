b29f947a0973f1b4df8be89a43f0c627e0e98252
Patrick Ng <patrickng995@gmail.com>
2025-11-18 23:12:19 -0800

    Updated default environment and asset names for saved level progress

--- Assets/Scripts/Persistence/SavedLevelProgressData.cs
+++ Assets/Scripts/Persistence/SavedLevelProgressData.cs
@@ -7,10 +7,13 @@ public class SavedLevelProgressData
     public string CurrentLevelAssetsName;
     public LevelProgressInfo[] CompletedLevelProgressInfoArray;
 
+    private const string DEFAULT_LEVEL_ENVIRONMENT_NAME = "2-office";
+    private const string DEFAULT_LEVEL_ASSETS_NAME = "2-office-easy";
+
     public SavedLevelProgressData()
     {
-        CurrentLevelEnvironmentName = "";
-        CurrentLevelAssetsName = "";
+        CurrentLevelEnvironmentName = DEFAULT_LEVEL_ENVIRONMENT_NAME;
+        CurrentLevelAssetsName = DEFAULT_LEVEL_ASSETS_NAME;
         CompletedLevelProgressInfoArray = new LevelProgressInfo[0];
     }
 }
