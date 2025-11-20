74d57c3b6dc151d5802d38bec8557b363a4aafea
Patrick Ng <patrickng995@gmail.com>
2025-11-19 16:02:55 -0800

    Added static grenade trail and update bullet trails to use line renderers

---
+++ Assets/Scripts/Objects/ToggleObjectOnUnpause.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ToggleObjectOnUnpause : MonoBehaviour, IPausable
+{
+    void IPausable.Pause()
+    {
+        gameObject.SetActive(true);
+    }
+
+    void IPausable.Unpause()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void IPausable.ResetStateBeforeUnpause()
+    {
+        gameObject.SetActive(true);
+    }
+}
